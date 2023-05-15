26581ca7db590ca324d47a4b7397b742772f0997
Luca Bandini <luca.vombato@gmail.com>
2023-05-15 20:53:17 +0200

    feat: changed scrape method

--- exposer.py
+++ exposer.py
@@ -8,23 +8,21 @@ load_dotenv(find_dotenv())
 LOCATION = os.environ.get("LOCATION")
 
 # Define a Prometheus gauge metric
-metric = Gauge(LOCATION, 'My metric description', ['time'])
+metric = Gauge(LOCATION, 'My metric description', ['source'])
 
 # Start the Prometheus HTTP server
 start_http_server(8001)
 
-# Read the data from the file
-with open('data.txt', 'r') as f:
-    data = f.readlines()
-
-# Loop through the data and update the metric
-for line in data:
-    parts = line.strip().split(';')
-    metric.labels(time=parts[1].strip()).set(float(parts[0].strip()))
-
-# Wait for the HTTP server to start up
-time.sleep(1)
-
-# Keep the script running
 while True:
-    time.sleep(1)
+
+    # Read the last line of the data file
+    with open('data.txt', 'r') as file:
+        for line in file:
+            pass
+    
+    # Split the line into two parts
+    parts = line.strip().split(';')
+    metric.labels(source="arpae").set(float(parts[0].strip()))
+
+    # Wait for the HTTP server to start up
+    time.sleep(10)
