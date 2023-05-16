6eea0fe80ab2421b44c423b757aa2c2308abaaa4
Luca Bandini <luca.vombato@gmail.com>
2023-05-16 09:13:05 +0200

    fix: surrounded by try except

--- exposer.py
+++ exposer.py
@@ -23,15 +23,19 @@ start_http_server(8001)
 
 while True:
 
-    for n in range(1, int(NUMBER)+1):
-        # Read the last line of the data file
-        with open(DBS[n-1], 'r') as file:
-            for line in file:
-                pass
-    
-        # Split the line into two parts
-        parts = line.strip().split(';')
-        METRICS[n-1].labels(source="arpae").set(float(parts[0].strip()))
+    try:
 
+        for n in range(1, int(NUMBER)+1):
+            # Read the last line of the data file
+            with open(DBS[n-1], 'r') as file:
+                for line in file:
+                    pass
+        
+            # Split the line into two parts
+            parts = line.strip().split(';')
+            METRICS[n-1].labels(source="arpae").set(float(parts[0].strip()))
+    except Exception as e:
+        print("Error: " + str(e))
+    
     # Wait for the HTTP server to start up
     time.sleep(10)
